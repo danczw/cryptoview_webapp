2c5919368265faf0cc9c6d2dbe582ee815d01dd0
danczw <devdancz@pm.me>
2021-05-16 16:41:29 +0200

    added vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    quoteData: {},
+    metaData: {},
+    transfData: {"name": "crypto", "children": []}
   },
   mutations: {
+    setQuoteData(state, _newData) {
+      state.quoteData = _newData;
+    },
+    setMetaData(state, _newData) {
+      state.metaData = _newData;
+    },
+    addTransfData(state, _newData) {
+      state.transfData.children.push(_newData);
+    },
+    resetTransfData(state) {
+      state.transfData = {"name": "crypto", "children": []};
+    }
   },
   actions: {
   },
