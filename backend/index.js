9460f89fdba90f1f58d8a577f5e5d07166a23f9f
danczw <devdancz@pm.me>
2021-05-16 21:40:00 +0200

    implemented backend for API calls

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,57 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const rp = require("request-promise");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (request, response) => {
-  console.log("query successful")
+app.get('/quotes/', (request, response) => {
+    console.log(request.query)
+    response.set("Access-Control-Allow-Origin", "*")
+    const cmc_key = process.env.CMC_KEY;
+    const requestOptions = {
+        method: "GET",
+        uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+        qs: {
+            "start": "1",
+            "limit": "3",
+            "convert": "USD"
+        },
+        headers: {
+            "X-CMC_PRO_API_KEY": cmc_key,
+        },
+        json: true,
+        gzip: true
+    };
+    rp(requestOptions).then(res => {
+        response.send(res.data);
+    }).catch((err) => {
+        console.log(err.message);
+    });
+})
+
+app.get('/meta/', (request, response) => {
+    console.log(request.query)
+    response.set("Access-Control-Allow-Origin", "*")
+    const cmc_key = process.env.CMC_KEY;
+    const requestOptions = {
+        method: "GET",
+        uri: "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest",
+        qs: {
+            "convert": "USD"
+        },
+        headers: {
+            "X-CMC_PRO_API_KEY": cmc_key,
+        },
+        json: true,
+        gzip: true
+    };
+    rp(requestOptions).then(res => {
+        response.send(res.data);
+    }).catch((err) => {
+        console.log(err.message);
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
-
-// const cmc_key = process.env.VUE_APP_COIN;
-// const rp = require("request-promise");
-// const requestOptions = {
-// method: "GET",
-// uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-// qs: {
-//     "start": "1",
-//     "limit": "3",
-//     "convert": "USD"
-// },
-// headers: {
-//     "X-CMC_PRO_API_KEY": cmc_key,
-// },
-// json: true,
-// gzip: true
-// };
