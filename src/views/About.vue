d4cbe88fd3773107059eb623b311d46a9d821a5f
danczw <devdancz@pm.me>
2021-05-20 23:51:58 +0200

    added basic tipping info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-wrapper">
+      <h3>Keep this website and my passion running</h3>
+      <p>Tipp Nano: nano_3jpa68851np7yzfwdzentxzr1rai9eftni4sabxogrruttjws7y1mhhrqu7c</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "./src/styles/variables.scss";
+.about {
+  color: $black-shadows
+}
+
+.about-wrapper {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  transform: -webkit-translate(-50%, -50%);
+  transform: -moz-translate(-50%, -50%);
+  transform: -ms-translate(-50%, -50%);
+}
+</style>
